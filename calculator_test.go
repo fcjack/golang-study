cde8088c3451d75fd6e68971c724a2641f00e551
Jackson Coelho <fcjack@gmail.com>
2021-09-26 15:23:25 +0200

    Add tests

--- calculator_test.go
+++ calculator_test.go
@@ -7,3 +7,9 @@ func TestAdd(t *testing.T) {
 		t.Errorf("add method produced wrong result. expected: %d, got: %d", want, got)
 	}
 }
+
+func TestSubtract(t *testing.T) {
+	if got, want := Subtract(5, 2), 3; got != want {
+		t.Errorf("subtract method produced wrong result. expected: %d, got: %d", want, got)
+	}
+}
